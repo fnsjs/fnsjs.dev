83839bbfbc678afe7c0db812bfdef9bf99c3b772
Jalal azimi <jalalazimi@gmail.com>
2020-10-05 22:23:42 +0100

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/docs":["static\u002Fchunks\u002Fpages\u002Fdocs.js"],"/docs/[slug]":["static\u002Fchunks\u002Fpages\u002Fdocs\u002F[slug].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
